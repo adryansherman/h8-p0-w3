005a1273f0f6a4ee54b3b8424fc450483e7130d0
adryansherman <adryansherman@gmail.com>
2019-08-06 22:10:11 +0700

    revisi ex-8

--- exercise-8.js
+++ exercise-8.js
@@ -4,8 +4,9 @@ function pasanganTerbesar(num) {
     var pasangan = '';
     var stringNum = String(num);
     for(var i = 0; i < stringNum.length; i++){
-        if (Number(stringNum[i] + stringNum[i+1])> pasangan){
-            pasangan = Number(stringNum[i] + stringNum[i+1]);
+    var urutanPasangan = Number(stringNum[i] + stringNum[i+1]);
+        if (urutanPasangan > pasangan){
+            pasangan = urutanPasangan;
         }
   }
   return pasangan
