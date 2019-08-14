ab420f56527b545627d6f3aa57960dd770eee3be
adryansherman <adryansherman@gmail.com>
2019-08-14 15:29:12 +0700

    revisi 13

--- exercise-13.js
+++ exercise-13.js
@@ -1,6 +1,6 @@
 function targetTerdekat(arr) {  
   var jarakXO = 0;
-  var tampungJarakXO = [];
+  var urutJarakXO = [];
   var indexX = arr.indexOf('x');//pencari ada ga x di array
   var indexO = arr.indexOf('o');//pencari lokasi o  
   
@@ -11,12 +11,13 @@ function targetTerdekat(arr) {
   for (var i = 0; i < arr.length; i ++){
     if (arr[i] === 'x'){
       jarakXO = Math.abs(i - indexO);
-      tampungJarakXO.push(jarakXO); //udah terurut dari yang paling pendek
+      urutJarakXO.push(jarakXO);
+      urutJarakXO.sort()
       
     }
   }
   
-  return tampungJarakXO[0] //ambil index-0
+  return urutJarakXO[0] //ambil index-0
 
 }
 console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
@@ -24,4 +25,5 @@ console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
 console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
 console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
 console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
-console.log(targetTerdekat([' ', ' ',' ', 'x', 'x', ' ', ' ', 'x'])); // 0
+console.log(targetTerdekat([' ', ' ',' ', 'x', 'x', ' ', ' ', 'x']));
+console.log(targetTerdekat([' ', ' ',' x', 'x', 'x', ' ', ' ', 'o']));
